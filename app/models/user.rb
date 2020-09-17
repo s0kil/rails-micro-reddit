c69ac5c1b937f4bca5ca3e1d2e60edbb247373a2
Elias Castañeda <e71az@icloud.com>
2020-09-17 12:09:16 -0600

    Created User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+    validates :user_name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum: 50}
+    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum: 50}, format: { with: ConstantData::VALID_EMAIL_REGEX }
+    validates :password, presence: true, length: {minimum: 5, maximum: 50}
 end
