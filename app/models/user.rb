2110650898d22a7d63d4db812ad159b53e5e9166
s0kil <dtechtours@gmail.com>
2020-09-17 15:51:17 -0400

    User Model Validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
-    validates :user_name, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum: 50}
-    validates :email, presence: true, uniqueness: { case_sensitive: false }, length: {minimum: 5, maximum: 50}, format: { with: ConstantData::VALID_EMAIL_REGEX }
-    validates :password, presence: true, length: {minimum: 5, maximum: 50}
+  has_many :posts
+  has_many :comments
+
+  validates :user_name, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 50 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 5, maximum: 50 }
+  validates :password, presence: true, length: { minimum: 5, maximum: 50 }
 end
