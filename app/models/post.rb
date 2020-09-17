b95a19777eeb8402f907f83389435216b348c813
s0kil <dtechtours@gmail.com>
2020-09-17 15:51:12 -0400

    Post Model Validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+
+  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
+  validates :body, presence: true
 end
